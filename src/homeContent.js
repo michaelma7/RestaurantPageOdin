555860cad437dffb56c8c438c3b60ed40cfc18d7
michaelma7 <michaelma16688@gmail.com>
2024-02-28 13:32:48 -0500

    1.0

--- src/homeContent.js
+++ src/homeContent.js
@@ -21,19 +21,19 @@ function homeContent () {
     reviews.lastChild.innerHTML = 'I love these muffins! They are always fresh and magma hot!'
     reviews.appendChild(document.createElement('p'));
     reviews.lastChild.classList.add('reviewer');
-    reviews.lastChild.innerHTML = '-woahbuddy';
+    reviews.lastChild.innerHTML = '-Mom';
 
     hours.appendChild(document.createElement('div'));
     hours.lastChild.classList.add('weekdays');
     hours.lastChild.appendChild(document.createElement('p'));
-    hours.lastChild.lastChild.innerHTML = 'Mon-Fri';
+    hours.lastChild.lastChild.innerHTML = 'Mon-Fri:';
     hours.lastChild.appendChild(document.createElement('p'));
     hours.lastChild.lastChild.innerHTML = '7 a.m - 4 p.m';
 
     hours.appendChild(document.createElement('div'));
     hours.lastChild.classList.add('weekends');
     hours.lastChild.appendChild(document.createElement('p'));
-    hours.lastChild.lastChild.innerHTML = 'Saturday and Sunday';
+    hours.lastChild.lastChild.innerHTML = 'Saturday and Sunday:';
     hours.lastChild.appendChild(document.createElement('p'));
     hours.lastChild.lastChild.innerHTML = '9 a.m - 3 p.m';
 
